1f0cb09caa08030c1b675a6b29d23e8f4c1cfd7f
paozanwa22 <fausanwadi@gmail.com>
2021-07-17 17:54:42 +0800

    Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,58 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+
+  void tambah() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
+  void kurang() {
+    setState(() {
+      number = number - 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Latihan Container'),),
-        body: Container(
-          color: Colors.red,
-          // margin: EdgeInsets.all(10),
-          // margin: EdgeInsets.fromLTRB(1, 15, 30, 40),
-          padding: EdgeInsets.only(bottom: 20, top: 20),
-          child: Container(
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: <Color>[
-                Colors.amber,
-                Colors.blue,
-            ])),
+        appBar: AppBar(
+          title: Text('stateful widget Demo'),
+        ),
+        body: Center(
+          child: Column(            
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 10 + number.toDouble()),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: ElevatedButton(
+                  onPressed: tambah,
+                  child: Text('Tambah'),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: ElevatedButton(
+                  onPressed: kurang,
+                  child: Text('Kurang'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
