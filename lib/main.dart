f086d2dde6afdc9efed6c4e785f4bd9090645f51
paozanwa22 <fausanwadi@gmail.com>
2021-07-17 11:00:30 +0800

    column & row

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("App v1"),
+        appBar: AppBar(title: Text('Latihan Row dan Column'),),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Text 1"),
+            Text(" Text 2"),
+            Row(
+              children: <Widget>[
+                Text("Text 3"),
+                Text(" Text 4"),
+              ],
+            ),
+          ],
         ),
-        body: Center(
-            child: Container(
-                color: Colors.lightBlue,
-                width: 150,
-                height: 130,
-                child: Text(
-                  "Saya sedang melatih kemampuan flutter saya",
-                  // maxLines: 2,
-                  // overflow: TextOverflow.clip,
-                  // softWrap: false
-                  // textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white, 
-                    fontStyle: FontStyle.italic, 
-                    fontWeight: FontWeight.w700, 
-                    fontSize: 20),
-                ))),
       ),
     );
   }
