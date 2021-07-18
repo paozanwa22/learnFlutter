9c373fdeacb06256e2051ec936ce73d9b01a6ee2
paozanwa22 <fausanwadi@gmail.com>
2021-07-19 02:46:29 +0800

    kalkulator v1

--- lib/main.dart
+++ lib/main.dart
@@ -12,48 +12,79 @@ class _MyAppState extends State<MyApp> {
 
   int counter = 1;
 
+  TextEditingController _inputController = TextEditingController(text: "");
+
+  List<String> _buttons = [
+    "7",
+    "8",
+    "9",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "x",
+    ",",
+    "0",
+    "=",
+    "/",
+  ];
+
+  void onChange(String text) {
+    this._inputController.text = this._inputController.text + text;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Anonymous Method'),
-        ),
-        body: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  child: Text('Tambah data'),
-                  onPressed: () {
-                    setState(() {
-                      widgets.add(Text(
-                        'data ke-' + counter.toString(),
-                        style: TextStyle(fontSize: 35),
-                      ));
-                      counter++;
-                    });
-                  },
-                ),
-                ElevatedButton(
-                  child: Text('Hapus data'),
-                  onPressed: () {
-                    setState(() {
-                      widgets.removeLast();
-                      counter--;
-                    });
-                  },
-                ),
-              ],
+          appBar: AppBar(
+            title: Text('Kalkulator'),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: this._inputController,
+                    cursorColor: Colors.green,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.clear),
+                        onPressed: () => this._inputController.clear(),
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0)),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    child: Padding(                  
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: GridView.builder(
+                          shrinkWrap: true,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 4),
+                          itemCount: _buttons.length,
+                          itemBuilder: (_, index) => Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  this.onChange(_buttons[index]);
+                                },
+                                child: Text(_buttons[index], style: TextStyle(fontSize: 20),)),
+                          )),
+                    ),
+                  )
+                ],
+              ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: widgets,
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
